26138daea5488c5804e5959487d751bcf03dd7fa
Jan van Lindert <Jan.vanLindert@wur.nl>
2023-04-08 11:36:18 +0200

    JVL
    Added some more example tiles to the board

--- Catan/src/board/Board.java
+++ Catan/src/board/Board.java
@@ -17,18 +17,24 @@ public class Board extends Canvas{ //extends Canvas is needed to draw, we think
   
         Toolkit t=Toolkit.getDefaultToolkit();  //This does something, just leave it 
         // with this we define an object as an image and give the file path. The pictures are already in the java project folder
-        Image Img_brick=t.getImage("TileImages/Tile_brick.png");  
-        Image Img_wood=t.getImage("TileImages/Tile_Forest.png");
+        Image Img_brick=t.getImage("TileImages/Tile_Brick.png");  
+        Image Img_wood=t.getImage("TileImages/Tile_Wood.png");
         Image Img_ore=t.getImage("TileImages/Tile_Ore.png");
-        Image Img_wool=t.getImage("TileImages/Tile_Sheep.png");
+        Image Img_wool=t.getImage("TileImages/Tile_Wool.png");
         Image Img_wheat=t.getImage("TileImages/Tile_Wheat.png");
         Image Img_desert=t.getImage("TileImages/Tile_Desert.png");
         Image Img_sea=t.getImage("TileImages/Tile_Sea.png");
         
         // draw the images: name, x, y, size_x, size_y
+        // the location of the tiles currently depends on your screen ratio
+        g.drawImage(Img_desert, 600, 250, 100, 100, this);
+        g.drawImage(Img_wood, 550, 175, 100, 100, this);
+        g.drawImage(Img_brick, 650, 175, 100, 100, this);
+        g.drawImage(Img_wool, 550, 325, 100, 100, this);
+        g.drawImage(Img_ore, 650, 325, 100, 100, this);
+        g.drawImage(Img_wheat, 500, 250, 100, 100, this);
+        g.drawImage(Img_wood, 700, 250, 100, 100, this);
 
-        g.drawImage(Img_desert, 592, 250, 100, 100, this); //max_x = 1182 max_y = ...
-        
           
     }  
 		// this generates the board in a JFrame
