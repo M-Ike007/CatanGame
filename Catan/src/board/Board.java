3281ddad6cd8370bac5b43dc11b78d2b3da0abc7
Jan van Lindert <Jan.vanLindert@wur.nl>
2023-04-11 17:04:46 +0200

    JVT IVV: removed redundant line
    removed "f.setState(Frame.NORMAL);" from board.java

--- Catan/src/board/Board.java
+++ Catan/src/board/Board.java
@@ -108,7 +108,6 @@ public class Board extends Canvas{ //extends Canvas is needed to draw, we think
         Board m=new Board();  
         JFrame f=new JFrame();  
         f.add(m); // adds the board to the JFrame
-        f.setState(Frame.NORMAL);
         f.setSize(1920,1080);  // sets the size of the JFrame window (1920x1080 is full screen (usually))
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setResizable(false);
