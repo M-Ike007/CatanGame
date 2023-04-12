68bd189ead40be1855bb088c1fe2ecd4882920a2
Sibbe Bakker <sibbe.bakker@wur.nl>
2023-04-12 17:20:44 +0200

    SB, GH, made test for the getStructureCost method

--- Catan/src/cost_card/CostTableTest.java
+++ Catan/src/cost_card/CostTableTest.java
@@ -1,12 +1,30 @@
 package cost_card;
 
 import junit.framework.TestCase;
+import java.util.*;
 
 public class CostTableTest extends TestCase {
 	
+	/** Testing the creation of the class.
+	*/
 	public void testCreation() {
 		CostTable costTest = new CostTable();
 		assertNotNull(costTest);
 	}
 	
+	/** Testing the values of the village.
+	 */
+	public void testgetStructureCost() {
+		Hashtable<String, Integer> expected_village = new Hashtable<String, Integer>();
+		expected_village.put("wood", 1);
+		expected_village.put("brick", 1);
+		expected_village.put("wool", 1);
+		expected_village.put("wheat", 1);
+		CostTable costTest = new CostTable();
+		Hashtable<String, Integer> actual = costTest.getStructureCost(Structures.VILLAGE);
+		assertEquals(actual.get("wood"), actual.get("wood"));
+		assertEquals(actual.get("brick"), actual.get("brick"));
+		assertEquals(actual.get("wool"), actual.get("wool"));
+		assertEquals(actual.get("weat"), actual.get("weat"));
+	}
 }
