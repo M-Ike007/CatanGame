da950427d8cca21c77fa1d5f1b8754314bbfcc03
Sam de Haan <sam.dehaan@wur.nl>
2023-04-17 17:13:11 +0200

    SDH, GH: add HexGridCell and HexLightsOut classes
    Start of code for displaying numbers on the board. Working in a new temp_board class that TEK,SB and LAK are working on.
    This class has not been comitted and pushed yet.

---
+++ Catan/src/temp_board/HexLightsOut.java
@@ -0,0 +1,119 @@
+package temp_board;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.swing.JPanel;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import java.awt.*;
+
+public class HexLightsOut extends JPanel{
+	
+	public HexLightsOut() {
+		int[][] cornersX = new int[BOARD_WIDTH * BOARD_HEIGHT][NUM_HEX_CORNERS];
+		int[][] cornersY = new int[BOARD_WIDTH * BOARD_HEIGHT][NUM_HEX_CORNERS];
+		int n = 0;
+	    for (int j = 0; j < BOARD_HEIGHT; j++) {
+	        for (int i = 0; i < BOARD_WIDTH; i++) {
+	            mCellMetrics.setCellIndex(i, j);
+            	mCellMetrics.computeCorners(mCornersX, mCornersY);
+            	cornersX[n] = Arrays.copyOf(mCornersX, mCornersX.length);
+                cornersY[n] = Arrays.copyOf(mCornersY, mCornersY.length);
+            	n ++;
+	        }
+	    }
+	    this.allcorX = cornersX;
+	    this.allcorY = cornersY;
+	}
+	
+	private static final long serialVersionUID = 1L;
+
+    private static final int BOARD_WIDTH = 2;
+    private static final int BOARD_HEIGHT = 3;
+
+    private static final int NUM_HEX_CORNERS = 6;
+    private static final int CELL_RADIUS = 40;
+   
+    public int[] mCornersX = new int[NUM_HEX_CORNERS];
+    public int[] mCornersY = new int[NUM_HEX_CORNERS];
+    
+    public int[][] allcorX;
+    public int[][] allcorY;
+   
+
+    public HexGridCell mCellMetrics = new HexGridCell(CELL_RADIUS);
+
+    private BufferedImage loadImage(File file) {
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+    
+    public void paint(Graphics g) {
+        for (int n = 0; n < BOARD_HEIGHT * BOARD_WIDTH; n++) {
+            File file = new File("hex2.png"); 
+            Polygon poly = new Polygon(allcorX[n], allcorY[n], 6);
+            BufferedImage image = loadImage(file);
+
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setClip(poly); 
+            g2d.fillPolygon(allcorX[n], allcorY[n], NUM_HEX_CORNERS);
+            g2d.drawImage(image, allcorX[n][0], allcorY[n][0] - mCellMetrics.RADIUS/2, null); // shift image 0.5* radius 
+
+            g2d.setColor(Color.BLACK);
+            g2d.drawPolygon(allcorX[n], allcorY[n], NUM_HEX_CORNERS);
+            g2d.setClip(null); 
+
+            // Display number inside hexagon tiles - SDH & GH (not done yet)
+            // Should pick numbers at random from the list given by numbers - now it just starts at the first number in the list
+            // Should display the number in the center of the hexagon - doesn't display it in the center yet 
+            // (try to use the getCenterY and getCenterX methods)
+            Font font = new Font("Arial", Font.PLAIN, 20);
+            g.setFont(font);
+            FontMetrics metrics = g.getFontMetrics(font);
+            int[] numbers = {1,2,2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12};
+            String number = String.valueOf(numbers[n]);
+            
+            int x = allcorX[n][0] + (mCellMetrics.RADIUS - metrics.stringWidth(number)) / 2;
+            int y = allcorY[n][0] + (mCellMetrics.RADIUS - metrics.getHeight()) / 2 + metrics.getAscent();
+            
+            g.setColor(Color.WHITE);
+            g.drawString(number, x, y);
+        }
+    }
+
+    public int[] getCornerX() {
+    	return mCornersX;
+    }
+    public int[] getCornerY() {
+    	return mCornersY;
+    }
+    public int[][] getallX(){
+    	return allcorX;
+    }
+    public int[][] getallY(){
+    	return allcorY;
+    }
+
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame("Hexagonal Grid");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        HexLightsOut hexLightsOut = new HexLightsOut();
+        frame.add(hexLightsOut);
+
+        frame.pack();
+        frame.setVisible(true);
+        System.out.println( Arrays.deepToString(hexLightsOut.allcorX));
+       
+  
+    }
+   
+}
