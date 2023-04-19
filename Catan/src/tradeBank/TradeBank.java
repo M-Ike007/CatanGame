05cd8566a6c9652bf32fd674eb3a854d43bcdd91
Getalem E. Haylia <getalemezezew.haylia@wur.nl>
2023-04-19 17:13:35 +0200

    GH & MI make button trade Bank
    we make a button to trade with bank. we also rename
    two classes because it makes more sense this way.

---
+++ Catan/src/tradeBank/TradeBank.java
@@ -0,0 +1,108 @@
+package tradeBank;
+
+import java.awt.event.*;  
+import javax.swing.*;
+import java.awt.*;
+
+import hand.*;
+import player.*;
+   
+/**
+ * @author TEK, SDH.
+ * The trade button the active player will see when it is their turn.
+ * 
+ * This class opens a window that asks the players whether they want to trade and if so what.
+ * Later future iterations should include how much of the given resources should be traded.
+ * The idea is to include arrows above the resources indicating the number to be traded.
+ * Then the active player can iterate over the non-active players to trade with.
+ * The non-active players can either return no, yes, or change request. 
+ * 
+ */
+
+
+
+public class TradeBank extends JFrame {
+	
+	public static void main(String[] args) {
+		TradeBank frame = new TradeBank();
+		//frame.setVisible(true);
+	}
+	
+	Hand hand = new Hand();
+	Player player = new Player(hand);
+	public void initializer() {
+	hand.setCard("resource", "wool", 6);
+	System.out.println(hand.getCard("resource", "wool"));
+	
+	}
+	
+	public TradeBank() {  
+		JFrame frame=new JFrame("Trade");  
+	    JTextField tf;
+	    tf = new JTextField("Do you want to trade?");
+	    JButton b1 =new JButton("Yes");  
+	    JButton b2 =new JButton("No");  
+	    b1.setBounds(50,200,300,30);
+	    b2.setBounds(350,200,300,30);
+	    tf.setEditable(false);
+	    tf.setBounds(50,100,600,30);
+	    b1.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				tf.setText("What do you want to give four resources of?");	
+				// five buttons of resources
+				JButton  wool =new JButton("wool"); 
+				JButton  ore =new JButton("ore");  
+				JButton  brick =new JButton("brick");  
+				JButton  wood =new JButton("wood");
+				JButton  wheat =new JButton("wheat");
+				// old buttons invisible
+				b1.setVisible(false); 
+				b2.setVisible(false); 
+				// set place and size of buttons
+				wool.setBounds(50,200,80,20);
+				ore.setBounds(150,200,80,20);
+				brick.setBounds(250,200,80,20);
+				wood.setBounds(350,200,80,20);
+				wheat.setBounds(450,200,80,20);
+				// add to JFrame
+				frame.add(wool);
+				frame.add(ore);
+				frame.add(brick);
+				frame.add(wood);
+				frame.add(wheat);
+			// add actions to buttons
+			wool.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					// remove four wool from hand
+					Banktax("resource", "wool", -4);
+					System.out.println(hand.getCard("resource", "wool"));
+					// create button to ask what the player wants to receive
+						
+					// actionlistener for giving that resource to the player
+					
+						
+					}
+					
+				});
+				
+			}
+	    	
+	    });  
+				
+	    frame.add(tf);
+	    frame.add(b1);
+	    frame.add(b2);
+	    frame.setSize(700,400);  
+	    frame.setLayout(null);
+	    frame.setVisible(true);
+	    
+	}  
+	
+	public void Banktax(String resource, String card, int amount) {
+		hand.setCard(resource, card, amount);
+		
+	
+	}
+	   
+}  	
+
---
+++ Catan/src/tradePlayer/TradePlayer.java
@@ -0,0 +1,141 @@
+package tradePlayer;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+import hand.Hand;
+import player.Player;
+
+/**
+ * 
+ * @author TEK, JVL
+ * 
+ *         In this class a GUI allows the active player to trade with the bank.
+ * 
+ *         The active player can add and remove cards from their hand by
+ *         clicking the corresponding buttons. When the button "ok" is clicked,
+ *         the cards are added and removed from their hand. For future coding
+ *         the Manager class should first check whether the trade is allowed.
+ */
+public class TradePlayer {
+
+	static JFrame frame;
+	static JButton buttonpl;
+	static JButton buttonmn;
+	static JButton ok;
+	static Integer wool;
+
+	public static void main(String[] args) {
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				frame = new JFrame("Trade with player");
+				Map<String, Integer> data = new LinkedHashMap<>();
+
+				Hand hand = new Hand();
+				Player player = new Player(hand);
+
+				JLabel label = new JLabel();
+				JLabel label2 = new JLabel();
+				JLabel label3 = new JLabel();
+				JLabel label4 = new JLabel();
+				JLabel label5 = new JLabel();
+				JLabel ok = new JLabel();
+
+				frame.setLayout(new FlowLayout());
+				Map<String, Integer> wool = buttonTrade(frame, "wool", label, data);
+				Map<String, Integer> wheat = buttonTrade(frame, "wheat", label2, data);
+				Map<String, Integer> wood = buttonTrade(frame, "wood", label3, data);
+				Map<String, Integer> ore = buttonTrade(frame, "ore", label4, data);
+				Map<String, Integer> brick = buttonTrade(frame, "brick", label5, data);
+
+				data.putAll(wool);
+				data.putAll(wheat);
+				data.putAll(wood);
+				data.putAll(ore);
+				data.putAll(brick);
+
+				tradeCards(frame, ok, player, wool);
+
+				frame.setVisible(true);
+				frame.setSize(700, 200);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			}
+		});
+
+	}
+
+	public static Map<String, Integer> buttonTrade(final JFrame frame1, String card, final JLabel label,
+			Map<String, Integer> data) {
+
+		final Integer[] score = { 0 };
+		frame1.getContentPane().setLayout(new FlowLayout());
+
+		buttonpl = new JButton("+");
+		buttonmn = new JButton("-");
+
+		JPanel panel = new JPanel();
+		frame1.add(panel);
+
+		buttonpl.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				score[0] += 1;
+				label.setText(card + ": " + score[0]);
+				frame1.pack();
+				data.put(card, score[0]);
+			}
+		});
+
+		buttonmn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				score[0] -= 1;
+//	        	if (score[0] < 0) {
+//	        		score[0] = 0;
+//	        	}
+				label.setText(card + ": " + score[0]);
+				frame1.pack();
+				data.put(card, score[0]);
+			}
+		});
+
+		frame1.add(label);
+		frame1.add(buttonpl);
+		frame1.add(buttonmn);
+		label.setPreferredSize(new Dimension(70, 50));
+		buttonpl.setPreferredSize(new Dimension(50, 30));
+		buttonmn.setPreferredSize(new Dimension(50, 30));
+		frame1.pack();
+		frame1.setVisible(true);
+		frame1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		return data;
+
+	}
+
+	public static void tradeCards(final JFrame frame1, final JLabel label, Player player, Map<String, Integer> amount) {
+
+		frame1.getContentPane().setLayout(new FlowLayout());
+
+		JPanel panel = new JPanel();
+		ok = new JButton("ok");
+		frame1.add(panel);
+
+		ok.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				label.setText("ok");
+				for (Map.Entry<String, Integer> set : amount.entrySet()) {
+					System.out.println(set.getKey());
+					player.hand.setCard("resource", set.getKey(), set.getValue());
+					int test = player.hand.getCard("resource", set.getKey());
+					System.out.println(set.getKey() + ": " + test);
+					frame1.pack();
+				}
+			}
+		});
+		frame1.add(label);
+		frame1.add(ok);
+		label.setPreferredSize(new Dimension(70, 50));
+		ok.setPreferredSize(new Dimension(50, 30));
+		frame1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	}
+}
