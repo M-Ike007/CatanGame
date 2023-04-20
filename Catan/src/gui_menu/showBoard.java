8b78f166cbefbf859cac9d2d4ae6c3febee75636
Sibbe Bakker <sibbe.bakker@wur.nl>
2023-04-20 17:29:42 +0200

    All finish refactoring
    Fix the error for running the board.

--- Catan/src/gui_menu/showBoard.java
+++ Catan/src/gui_menu/showBoard.java
@@ -6,8 +6,6 @@ import gui_board.*;
 
 import javax.swing.JButton;
 import java.awt.BorderLayout;
-import java.awt.Component;
-import javax.swing.JInternalFrame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -15,7 +13,6 @@ import java.awt.event.ActionListener;
 
 public class showBoard {
 	private JFrame f = new JFrame("Catan - Board");
-	private JInternalFrame internalf = new JInternalFrame();
 	private JLabel lNotYetImplemented = new JLabel("Not yet implemented");
 	private JButton bGoBack = new JButton("Back");
 	private Table TableTest = new Table();
@@ -27,12 +24,7 @@ public class showBoard {
 		
 		f.getContentPane().add(TableTest, BorderLayout.NORTH);
 		
-		Board m=new Board();  
-		internalf.add(m); // adds the board to the JFrame
-		internalf.setSize(1920,1080);  // sets the size of the JFrame window (1920x1080 is full screen (usually))
-		internalf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		internalf.setResizable(false);
-		internalf.setVisible(true); 
+		new Board();  
 		
 		bGoBack.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
