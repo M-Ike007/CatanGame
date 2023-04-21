0bab544b4d9bc63fec50758c44bfe141f7272954
titia kool <titia.kool@wur.nl>
2023-04-21 17:09:31 +0200

    TEK,IVV add LocationJunctionTest and modify LocationJunction.
    The class can now calculate all the x and y coordinates of one hexagon.

---
+++ Catan/src/location/LocationJunctionTest.java
@@ -0,0 +1,30 @@
+package location;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+
+public class LocationJunctionTest extends TestCase {
+	public void testGetX() {
+		LocationJunction loc = new LocationJunction();
+		ArrayList<Integer> x = loc.getCorX(450);
+		assertEquals(x.get(0), Integer.valueOf(445));
+		assertEquals(x.get(1), Integer.valueOf(495));
+		assertEquals(x.get(2), Integer.valueOf(545));
+		assertEquals(x.get(3), Integer.valueOf(545));
+		assertEquals(x.get(4), Integer.valueOf(495));
+		assertEquals(x.get(5), Integer.valueOf(445));
+		
+	}
+	public void testGetY() {
+		LocationJunction loc = new LocationJunction();
+		ArrayList<Integer> y = loc.getCorY(25);
+		assertEquals(y.get(0), Integer.valueOf(45));
+		assertEquals(y.get(1), Integer.valueOf(20));
+		assertEquals(y.get(2), Integer.valueOf(45));
+		assertEquals(y.get(3), Integer.valueOf(95));
+		assertEquals(y.get(4), Integer.valueOf(120));
+		assertEquals(y.get(5), Integer.valueOf(95));
+		
+	}
+}
