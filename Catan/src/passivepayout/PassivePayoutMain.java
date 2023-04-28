6365beef9152c7aa78b011e4a4a25769d8b72101
Inge van Vugt <inge.vanvugt@wur.nl>
2023-04-28 17:13:07 +0200

    IVV, GH: Rename PassivePayoutTest.java to PassivePayoutMain.java
    First it was a test, but we have now a JUnit test so it is changed to a main class.

--- Catan/src/passivepayout/PassivePayoutMain.java
+++ Catan/src/passivepayout/PassivePayoutMain.java
@@ -1,6 +1,6 @@
 package passivepayout;
 
-public class PassivePayoutTest {
+public class PassivePayoutMain {
 
 	public static void main(String[] args) {
 		PassivePayout test = new PassivePayout();
