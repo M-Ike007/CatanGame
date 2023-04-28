0c489f7ac32926e26d288538b04ae94d740a36d6
Sam de Haan <sam.dehaan@wur.nl>
2023-04-28 16:32:50 +0200

    SDH, TEK refactor trade_bank package name to tradebank.

--- Catan/src/guiboard/Board.java
+++ Catan/src/guiboard/Board.java
@@ -2,8 +2,8 @@ package guiboard;
 
 import java.awt.*;
 import location.LocationJunction;
-import trade_bank.TradeBank;
 import trade_player.TradePlayer;
+import tradebank.TradeBank;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
