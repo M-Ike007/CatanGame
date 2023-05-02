290b4c3b00e5a8bc4afbf46401cc17bce86ad918
Mees Ike <mees.ike@wur.nl>
2023-05-02 17:06:46 +0200

    MI, TEK write methods for DevCard and Invention.
    write methods for DvCard that can be used by Invention subclass.
    Also write testfile for these methods.

---
+++ Catan/src/test/DevInventionTest.java
@@ -0,0 +1,52 @@
+package test;
+
+import junit.framework.TestCase;
+import hand.Hand;
+import devcards.DevCard;
+import devcards.DevInvention;
+
+public class DevInventionTest extends TestCase {
+	public void testIncreaseTest() {
+		Hand testHand = new Hand();
+		DevInvention hi = new DevInvention();
+		testHand.setCard("resource", "wheat", 1);
+		testHand.setCard("resource", "ore", 1);
+		testHand.setCard("resource", "wool", 1);
+		
+		
+		DevCard testDevCard2 = new DevCard();
+		testDevCard2.increaseHand(testHand, "invention");
+		
+		hi.invent(testHand);
+		
+		int knight = testHand.getCard("development", "knight");
+		System.out.println(knight + "knight");
+		int twoRoads = testHand.getCard("development", "twoRoads");
+		System.out.println(twoRoads + "2road");
+		int invention = testHand.getCard("development", "invention");
+		System.out.println(invention + "invet");
+		int monopoly = testHand.getCard("development", "monopoly");
+		System.out.println(monopoly + "monop");
+		int vcp = testHand.getCard("development", "vcp");
+		System.out.println(vcp + "vcp");
+		
+		int wheat = testHand.getCard("resource", "wheat");
+		System.out.println(wheat + "wheat");
+		int ore = testHand.getCard("resource", "ore");
+		System.out.println(ore + "ore");
+		int wood = testHand.getCard("resource", "wood");
+		System.out.println(wood + "wood");
+		int brick = testHand.getCard("resource", "brick");
+		System.out.println(brick + "brick");
+		int wool = testHand.getCard("resource", "wool");
+		System.out.println(wool + "wool");
+		
+		int sum = knight + twoRoads + invention + monopoly + vcp + wheat + ore + wood + brick + wool;
+		System.out.println(sum);
+		
+		assertEquals(sum, 4);
+	}
+	
+	
+
+}
