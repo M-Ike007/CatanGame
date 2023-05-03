0f7e2c6b17c43c67e38b8901f29810a7c367357b
Jan van Lindert <Jan.vanLindert@wur.nl>
2023-05-03 15:20:18 +0200

    JVL SB refactor code to java conventions

--- Catan/src/devcards/DevVicPoint.java
+++ Catan/src/devcards/DevVicPoint.java
@@ -1,9 +1,10 @@
 package devcards;
+
 import hand.Hand;
 
-public class DevVicPoint extends DevCard{
+public class DevVicPoint extends DevCard {
 	DevVicPoint card = new DevVicPoint();
-	
+
 	public void increaseInvention(Hand myHand) {
 		card.increaseHand(myHand, "vcp");
 	}
