d6c72f22369f643a64d03102712388f0e507aea9
Jan van Lindert <Jan.vanLindert@wur.nl>
2023-05-03 15:18:41 +0200

    JVL SB refactor code to java conventions

--- Catan/src/devcards/DevInvention.java
+++ Catan/src/devcards/DevInvention.java
@@ -1,11 +1,13 @@
 package devcards;
+
 import hand.Hand;
 
-public class DevInvention extends DevCard{
-	
+public class DevInvention extends DevCard {
+
 	public void increaseInvention(DevCard card, Hand myHand) {
 		card.increaseHand(myHand, "invention");
 	}
+
 	@Override
 	public void invent(Hand myHand) {
 		can_invent = true;
@@ -17,16 +19,17 @@ public class DevInvention extends DevCard{
 			myHand.setCard("development", "invention", -1);
 		}
 	}
+
 	public static void main(String[] args) {
 		Hand hello = new Hand();
 		DevInvention hoi = new DevInvention();
 		hoi.invent(hello);
-		
+
 		System.out.println("wool:" + hello.getCard("resource", "wool"));
 		System.out.println("wood:" + hello.getCard("resource", "wood"));
 		System.out.println("ore:" + hello.getCard("resource", "ore"));
 		System.out.println("wheat:" + hello.getCard("resource", "wheat"));
 		System.out.println("brick:" + hello.getCard("resource", "brick"));
-		
+
 	}
 }
