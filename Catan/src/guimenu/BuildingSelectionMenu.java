179f5449d3ff00c71ef25cd68cd9dcc69ab96d9c
Inge van Vugt <inge.vanvugt@wur.nl>
2023-05-03 14:00:39 +0200

    IVV, LAK. Change to only close the build frame
    Before also the board frame closed when closing the build frame.

--- Catan/src/guimenu/BuildingSelectionMenu.java
+++ Catan/src/guimenu/BuildingSelectionMenu.java
@@ -38,7 +38,7 @@ public class BuildingSelectionMenu extends JFrame {
 	 * Create the frame.
 	 */
 	public BuildingSelectionMenu() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 447, 190);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
