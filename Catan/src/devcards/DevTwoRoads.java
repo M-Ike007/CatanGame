bad00b97818b7cbcd0335c50d81776ab9b6da89d
Jan van Lindert <Jan.vanLindert@wur.nl>
2023-05-03 15:37:55 +0200

    JVL SB remove recursion from all devcard classes that have recursion

--- Catan/src/devcards/DevTwoRoads.java
+++ Catan/src/devcards/DevTwoRoads.java
@@ -1,11 +1,5 @@
 package devcards;
 
-import hand.Hand;
-
 public class DevTwoRoads extends DevCard {
-	DevTwoRoads card = new DevTwoRoads();
 
-	public void increaseTwoRoads(Hand myHand) {
-		card.increaseHand(myHand, "twoRoads");
-	}
 }
