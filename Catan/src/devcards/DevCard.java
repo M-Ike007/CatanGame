778a27843983d4bfd00e32aa28d965753ad6e109
Sibbe Bakker <sibbe.bakker@wur.nl>
2023-05-03 15:34:24 +0200

    SB, JVL extract class from method.
    Move tests from one test class into a new one to match this change.

--- Catan/src/devcards/DevCard.java
+++ Catan/src/devcards/DevCard.java
@@ -8,42 +8,15 @@ public class DevCard {
 	private int result = 0;	
 	protected boolean can_invent = false;
 	private int resource_result = 0;
+	private Distribution dist = new Distribution();
 	/**
 	 * HashMap emulates the distribution of cards in the game.
 	 * right now there is infinite card so it is possible to draw more cards than are normally available (like 3 monopoly cards).
 	 * @Return random Development Card from the HashMap
 	 */
-	public String drawDevCard() {
-		Random generator = new Random();	
-		HashMap<Integer, String> distribution = new HashMap<Integer, String>();
-			distribution.put(1, "knight");
-			distribution.put(2, "knight");
-			distribution.put(3, "knight");
-			distribution.put(4, "knight");
-			distribution.put(5, "knight");
-			distribution.put(6, "knight");
-			distribution.put(7, "knight");
-			distribution.put(8, "knight");
-			distribution.put(9, "knight");
-			distribution.put(10, "knight");
-			distribution.put(11, "knight");
-			distribution.put(12, "knight");
-			distribution.put(13, "knight");
-			distribution.put(14, "knight");
-			distribution.put(15, "vcp");
-			distribution.put(16, "vcp");
-			distribution.put(17, "vcp");
-			distribution.put(18, "vcp");
-			distribution.put(19, "vcp");
-			distribution.put(20, "twoRoads");
-			distribution.put(21, "twoRoads");		
-			distribution.put(22, "invention");
-			distribution.put(23, "invention");
-			distribution.put(24, "monopoly");
-			distribution.put(25, "monopoly");		
-			result = generator.nextInt(1,26);
-			String drawnCard = distribution.get(result);	
-		return drawnCard;
+	public String drawDevCard() {	
+		this.dist.setResult();
+		return this.dist.getCard();
 	}
 	/**
 	 * increases development card in hand
