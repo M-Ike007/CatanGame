8e7526ab53c5428ab90c178c3d8a72507b14029f
Inge van Vugt <inge.vanvugt@wur.nl>
2023-05-03 15:59:20 +0200

    IVV, LAK. make Dice.java code adhere to standard layout

--- Catan/src/dice/Dice.java
+++ Catan/src/dice/Dice.java
@@ -5,8 +5,7 @@ import java.util.Random;
 /**
  * This class simulates two dice throws and sums the results.
  */
-public class Dice 
-{
+public class Dice {
 	private int die1;
 	private int die2;
 	private int sum;
@@ -14,20 +13,19 @@ public class Dice
 	/**
 	 * sets the sum of the two dice throws.
 	 */
-	public void setSum()
-	{
+	public void setSum() {
 		Random random = new Random();
-		die1 = random.nextInt(1,7);
-		die2 = random.nextInt(1,7);
+		die1 = random.nextInt(1, 7);
+		die2 = random.nextInt(1, 7);
 		this.sum = die1 + die2;
 	}
-	
+
 	/**
 	 * gets the sum of the two dice throws.
+	 * 
 	 * @return int: sum.
 	 */
-	public int getSum()
-	{
+	public int getSum() {
 		return sum;
 	}
 }
