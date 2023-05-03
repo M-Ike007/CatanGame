118d8249b5d64308634f99f690c5ff1db2429fc7
Sibbe Bakker <sibbe.bakker@wur.nl>
2023-05-03 17:21:44 +0200

    SB, JVL: Fix issues caused by previous refactor.

--- Catan/src/cards/DevInvention.java
+++ Catan/src/cards/DevInvention.java
@@ -4,8 +4,8 @@ import hand.Hand;
 
 public class DevInvention extends DevCard {
 
-	public void increaseInvention(DevCard card, Hand hand) {
-		card.increaseHand(hand, "invention");
+	public void transactDevelopmentCardPurchaseInvention(DevCard card, Hand hand) {
+		card.transactDevelopmentCardPurchase(hand, "invention");
 	}
 
 	/** The enabled version of the invent class.
@@ -13,11 +13,11 @@ public class DevInvention extends DevCard {
 	 * @param hand The to be given extra resources based on the invention rules. 
 	 */
 	@Override
-	public void invent(Hand hand) {
+	public void setInventReward(Hand hand) {
 		can_invent = true;
 		if (can_invent) {
-			hand.setCard("resource", draw_random_resource(), 1);
-			hand.setCard("resource", draw_random_resource(), 1);
+			hand.setCard("resource", getResourceDraw(), 1);
+			hand.setCard("resource", getResourceDraw(), 1);
 			hand.setCard("development", "invention", -1);
 		}
 	}
--- Catan/src/test/DevInventionTest.java
+++ Catan/src/test/DevInventionTest.java
@@ -15,9 +15,9 @@ public class DevInventionTest extends TestCase {
 		
 		
 		DevCard testDevCard2 = new DevCard();
-		testDevCard2.increaseHand(testHand, "invention");
+		testDevCard2.transactDevelopmentCardPurchase(testHand, "invention");
 		
-		hi.invent(testHand);
+		hi.setInventReward(testHand);
 		
 		int knight = testHand.getCard("development", "knight");
 		System.out.println(knight + "knight");
