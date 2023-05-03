b00472e5e9574dd9f1e74b330bb80b322ed02442
Jan van Lindert <Jan.vanLindert@wur.nl>
2023-05-03 15:18:05 +0200

    JVL SB refactor code to java conventions

--- Catan/src/devcards/DevCard.java
+++ Catan/src/devcards/DevCard.java
@@ -5,58 +5,64 @@ import java.util.Random;
 import hand.Hand;
 
 public class DevCard {
-	private int result = 0;	
+	private int result = 0;
 	protected boolean can_invent = false;
 	private int resource_result = 0;
+
 	/**
-	 * HashMap emulates the distribution of cards in the game.
-	 * right now there is infinite card so it is possible to draw more cards than are normally available (like 3 monopoly cards).
+	 * HashMap emulates the distribution of cards in the game. right now there is
+	 * infinite card so it is possible to draw more cards than are normally
+	 * available (like 3 monopoly cards).
+	 * 
 	 * @Return random Development Card from the HashMap
 	 */
 	public String drawDevCard() {
-		Random generator = new Random();	
+		Random generator = new Random();
 		HashMap<Integer, String> distribution = new HashMap<Integer, String>();
-			distribution.put(1, "knight");
-			distribution.put(2, "knight");
-			distribution.put(3, "knight");
-			distribution.put(4, "knight");
-			distribution.put(5, "knight");
-			distribution.put(6, "knight");
-			distribution.put(7, "knight");
-			distribution.put(8, "knight");
-			distribution.put(9, "knight");
-			distribution.put(10, "knight");
-			distribution.put(11, "knight");
-			distribution.put(12, "knight");
-			distribution.put(13, "knight");
-			distribution.put(14, "knight");
-			distribution.put(15, "vcp");
-			distribution.put(16, "vcp");
-			distribution.put(17, "vcp");
-			distribution.put(18, "vcp");
-			distribution.put(19, "vcp");
-			distribution.put(20, "twoRoads");
-			distribution.put(21, "twoRoads");		
-			distribution.put(22, "invention");
-			distribution.put(23, "invention");
-			distribution.put(24, "monopoly");
-			distribution.put(25, "monopoly");		
-			result = generator.nextInt(1,26);
-			String drawnCard = distribution.get(result);	
+		distribution.put(1, "knight");
+		distribution.put(2, "knight");
+		distribution.put(3, "knight");
+		distribution.put(4, "knight");
+		distribution.put(5, "knight");
+		distribution.put(6, "knight");
+		distribution.put(7, "knight");
+		distribution.put(8, "knight");
+		distribution.put(9, "knight");
+		distribution.put(10, "knight");
+		distribution.put(11, "knight");
+		distribution.put(12, "knight");
+		distribution.put(13, "knight");
+		distribution.put(14, "knight");
+		distribution.put(15, "vcp");
+		distribution.put(16, "vcp");
+		distribution.put(17, "vcp");
+		distribution.put(18, "vcp");
+		distribution.put(19, "vcp");
+		distribution.put(20, "twoRoads");
+		distribution.put(21, "twoRoads");
+		distribution.put(22, "invention");
+		distribution.put(23, "invention");
+		distribution.put(24, "monopoly");
+		distribution.put(25, "monopoly");
+		result = generator.nextInt(1, 26);
+		String drawnCard = distribution.get(result);
 		return drawnCard;
 	}
+
 	/**
 	 * increases development card in hand
+	 * 
 	 * @param myHand is the hand of the player.
-	 * @param card is the type development card.
+	 * @param card   is the type development card.
 	 */
-	public void increaseHand(Hand myHand, String card) {		
-		myHand.setCard("development",card, 1);
+	public void increaseHand(Hand myHand, String card) {
+		myHand.setCard("development", card, 1);
 		myHand.setCard("resource", "wool", -1);
 		myHand.setCard("resource", "wheat", -1);
 		myHand.setCard("resource", "ore", -1);
-		
+
 	}
+
 	public void invent(Hand myHand) {
 		if (can_invent) {
 			System.out.println("hi");
@@ -65,20 +71,18 @@ public class DevCard {
 			myHand.setCard("development", "invention", -1);
 		}
 	}
+
 	public String draw_random_resource() {
 		HashMap<Integer, String> resource = new HashMap<Integer, String>();
-		
+
 		resource.put(1, "wool");
 		resource.put(2, "ore");
 		resource.put(3, "wood");
 		resource.put(4, "brick");
 		resource.put(5, "wheat");
 		Random res_gen = new Random();
-		resource_result = res_gen.nextInt(1,6);
-		String drawn_resource = resource.get(resource_result);	
-	return drawn_resource;
+		resource_result = res_gen.nextInt(1, 6);
+		String drawn_resource = resource.get(resource_result);
+		return drawn_resource;
 	}
 }
-
-
-
