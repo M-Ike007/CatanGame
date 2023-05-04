2de4714984c45d057436da4b189197d3f608ac2b
Inge van Vugt <inge.vanvugt@wur.nl>
2023-05-04 15:15:42 +0200

    IVV, LAK. Board code stops running after 10 turns

--- Catan/src/guiboard/Board.java
+++ Catan/src/guiboard/Board.java
@@ -20,6 +20,7 @@ import game.TurnManager;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -803,7 +804,7 @@ public class Board {
 			}
 		}
 		else {
-			f.dispose();
+			f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
 		}
 	}
 	
