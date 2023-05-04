0468c5c9beccfc6f1927c51cf4ac628915fdef74
Inge van Vugt <inge.vanvugt@wur.nl>
2023-05-04 14:42:02 +0200

    IVV, LAK. Add java docstrings in TileInformation.java

--- Catan/src/passivepayout/TileInformation.java
+++ Catan/src/passivepayout/TileInformation.java
@@ -2,10 +2,13 @@ package passivepayout;
 
 import java.util.HashMap;
 import java.util.Map.Entry;
-
 import hand.*;
 import guiboard.TilesNumbers;
 
+/**
+ * This class makes the tile information for the passive payout and sets 
+ * the payout for a player. 
+ */
 public class TileInformation {
 	private TilesNumbers tilesNumbers = new TilesNumbers();
 	private HashMap<String, Integer> rNums;
@@ -13,17 +16,27 @@ public class TileInformation {
 	private HashMap<String, String> results = new HashMap<String, String>();
 	private HashMap<Integer, String> housesPlayers = new HashMap<Integer, String>();
 	private Hand hand;
-
+	
+	
+	/**
+	 * Sets the tile numbers information
+	 */
 	public void setTileNumber() {
 		tilesNumbers.setTileNumbers();
 		rNums = tilesNumbers.getTileNumbers();
 	}
 	
+	/**
+	 * Sets the houses on tiles of the players
+	 */
 	public void setHouseOnTile() {
 		housesPlayers.put(1, "Pos22-Pos33-Pos34-Pos54-Pos55-Pos64");
 		housesPlayers.put(2, "Pos35-Pos42-Pos43-Pos45-Pos52-Pos53");
 	}
 	
+	/**
+	 * Sets the resources of the tiles
+	 */
 	public void setResourceOfTile() {
 		resourceTile.put("Pos22", "wood");
 		resourceTile.put("Pos23", "wool");
@@ -45,12 +58,21 @@ public class TileInformation {
 		resourceTile.put("Pos64", "brick");
 	}
 	
+	/**
+	 * Sets the passive payout information
+	 */
 	public void setPassivePayoutInformation() {
 		for (Entry<String, Integer> set : rNums.entrySet()) {
 			results.put(set.getKey(), Integer.toString(rNums.get(set.getKey())) + '-' + resourceTile.get(set.getKey()));
 		}
 	}
 	
+	/**
+	 * Sets the payout for an player. 
+	 * @param number_dice: int, number of dice throw
+	 * @param hand: Hand, hand of the player
+	 * @param playerNumber: int, number of the player
+	 */
 	public void setTilesInformation(int number_dice, Hand hand, int playerNumber) {
 		String player1houses = housesPlayers.get(playerNumber);
 		String[] splitString = player1houses.split("-");
@@ -64,6 +86,10 @@ public class TileInformation {
 		this.hand = hand;
 	}
 	
+	/**
+	 * Gets the payout of the player
+	 * @return Hand hand of player
+	 */
 	public Hand getTilesInformation() {
 		return hand;
 	}
