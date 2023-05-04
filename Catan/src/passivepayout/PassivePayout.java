94f5390c6c63e0593e46827711a5a708bc475060
Inge van Vugt <inge.vanvugt@wur.nl>
2023-05-04 14:50:19 +0200

    IVV, LAK. adhere code to standard layout

--- Catan/src/passivepayout/PassivePayout.java
+++ Catan/src/passivepayout/PassivePayout.java
@@ -8,11 +8,12 @@ import hand.*;
 public class PassivePayout {
 	private TileInformation information = new TileInformation();
 	private Hand payoutResult;
-	
+
 	/**
 	 * Sets the payout of a player
-	 * @param dice_number: int, number of dice throw
-	 * @param hand: Hand, hand of a player 
+	 * 
+	 * @param dice_number:  int, number of dice throw
+	 * @param hand:         Hand, hand of a player
 	 * @param playerNumber: int, player number
 	 */
 	public void setPayout(int dice_number, Hand hand, int playerNumber) {
@@ -23,9 +24,10 @@ public class PassivePayout {
 		information.setTilesInformation(dice_number, hand, playerNumber);
 		payoutResult = information.getTilesInformation();
 	}
-	
+
 	/**
 	 * Gets the payout of a player
+	 * 
 	 * @return Hand:, hand of the player
 	 */
 	public Hand getPayout() {
