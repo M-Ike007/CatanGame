ee4e46872d54ab0cb3339ee5b52e04c93fc2ce97
Inge van Vugt <inge.vanvugt@wur.nl>
2023-05-04 14:44:05 +0200

    IVV, LAK. Add java docstrings in PassivePayout.java

--- Catan/src/passivepayout/PassivePayout.java
+++ Catan/src/passivepayout/PassivePayout.java
@@ -2,10 +2,19 @@ package passivepayout;
 
 import hand.*;
 
+/**
+ * Sets the passive payout of a player.
+ */
 public class PassivePayout {
 	private TileInformation information = new TileInformation();
 	private Hand payout_result;
 	
+	/**
+	 * Sets the payout of a player
+	 * @param dice_number: int, number of dice throw
+	 * @param hand: Hand, hand of a player 
+	 * @param playerNumber: int, player number
+	 */
 	public void SetPayout(int dice_number, Hand hand, int playerNumber) {
 		information.setTileNumber();
 		information.setResourceOfTile();
@@ -15,6 +24,10 @@ public class PassivePayout {
 		payout_result = information.getTilesInformation();
 	}
 	
+	/**
+	 * Gets the payout of a player
+	 * @return Hand:, hand of the player
+	 */
 	public Hand GetPayout() {
 		return payout_result;
 	}
